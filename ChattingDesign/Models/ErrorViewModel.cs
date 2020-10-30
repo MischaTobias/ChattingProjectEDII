b163d988ec249c0bdb21268b0123a0de7629a0dc
MischaTobias <mischa.tobias4b@gmail.com>
2020-10-30 14:57:34 -0600

    All layers created, working on them is still pending

---
+++ ChattingDesign/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChattingDesign.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
